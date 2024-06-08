2ec24a098fb80bb9dc446a2a3c439c0c5359cb43
Christianib003 <christianib003@gmail.com>
2024-06-08 17:32:30 +0200

    add test case for hello_world() route

---
+++ test_app.py
@@ -0,0 +1,48 @@
+"""
+Module for testing the app.py
+"""
+
+import unittest
+from app import app
+
+
+class HelloWorldTestCase(unittest.TestCase):
+    """
+    Test case for the HelloWorld endpoint in the Flask app.
+    """
+
+    def setUp(self):
+        """
+        Set up the test environment before each test case.
+
+        This method is called automatically before each test case is executed.
+        It initializes the Flask test client and sets the testing flag to True.
+
+        """
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_hello_world(self):
+        """
+        Test case for the hello world endpoint.
+
+        This method sends a GET request to the '/hello' endpoint and asserts that the response
+        status code is 200 and the response data is 'Hello, World!'.
+
+        """
+        response = self.app.get('/hello')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), 'Hello, World!')
+
+    def tearDown(self):
+        """
+        Clean up the test environment after each test case.
+
+        This method is called automatically after each test case is executed.
+
+        """
+        del self.app
+
+
+if __name__ == '__main__':
+    unittest.main()
