18ae4d6ccc38cbfd8c2dea7d93b5dca24c6f6a3d
Christianib003 <christianib003@gmail.com>
2024-06-09 14:30:36 +0200

    update the path of swagger-ui docs

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config["API_TITLE"] = "EcoTrack API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui/api/docs"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
