b37e39819b6e35faaa27fddca6f5f7984a01b341
Christianib003 <christianib003@gmail.com>
2024-06-09 13:38:48 +0200

    create blueprint to handle endpoints of household(s)

---
+++ resources/household.py
@@ -0,0 +1,90 @@
+"""
+Blueprint for household resources
+"""
+
+import uuid
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import households
+
+
+blp = Blueprint(
+    "households",
+    __name__,
+    description="Operations on households"
+    )
+
+
+@blp.route("/households")
+class Households(MethodView):
+    """
+    Class for handling requests to the /households endpoint
+    """
+    def get(self):
+        """
+        Get all households in the database
+
+        Returns:
+            dict: A dictionary containing all households in the database
+        """
+        return {"households": list(households.values())}
+
+    def post(self):
+        """
+        Add a new household to the database
+
+        Returns:
+            tuple: A tuple containing the newly added household and the
+            HTTP status code 201
+        """
+        household_data = request.get_json()
+        household_id = uuid.uuid4().hex
+        new_household = {**household_data, "id": household_id}
+        households[household_id] = new_household
+        return new_household, 201
+
+
+@blp.route("/households/<household_id>")
+class Household(MethodView):
+    """
+    Class for handling requests to the /households/<household_id> endpoint
+    """
+    def get(self, household_id):
+        """
+        Get a household by ID
+
+        Args:
+            household_id (str): The ID of the household to retrieve
+
+        Returns:
+            tuple: A tuple containing the requested household and the
+            HTTP status code 200
+
+        Raises:
+            404: If the household with the given ID is not found
+        """
+        try:
+            return households[household_id], 200
+        except KeyError:
+            abort(404, message="Household not found.")
+
+    def delete(self, household_id):
+        """
+        Delete a household by ID
+
+        Args:
+            household_id (str): The ID of the household to delete
+
+        Returns:
+            tuple: A tuple containing a message indicating the deletion and
+            the HTTP status code 200
+
+        Raises:
+            404: If the household with the given ID is not found
+        """
+        try:
+            del households[household_id]
+            return {"message": "Household deleted"}, 200
+        except KeyError:
+            abort(404, message="Household not found.")
