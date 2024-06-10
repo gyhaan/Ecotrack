d3f842d2421abce716c0a0e5de1e57d8313b3a30
Christianib003 <christianib003@gmail.com>
2024-06-10 12:32:45 +0200

    create admin model

---
+++ models/admin.py
@@ -0,0 +1,17 @@
+"""
+This module contains the Admin model
+"""
+
+from db import db
+
+
+class AdminModel(db.Model):
+    """
+    This class represents an admins table in the database.
+    """
+
+    __tablename__ = "admins"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), unique=True, nullable=False)
+    user = db.relationship("UserModel", back_populates="admin", uselist=False)
